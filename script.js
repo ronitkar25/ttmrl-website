e00cd59e2a8799e6b9414e12ca0302626fd2a37f
ronitkar25 <rok73@pitt.edu>
2024-12-22 22:49:32 -0500

    testing flexible home page on our team page temporarily

---
+++ script.js
@@ -0,0 +1,5 @@
+document.getElementById('hamburger').addEventListener('click', function () {
+    const navLinks = document.getElementById('navLinks');
+    navLinks.classList.toggle('active');
+    this.textContent = this.textContent === '☰' ? '✖' : '☰';
+});
